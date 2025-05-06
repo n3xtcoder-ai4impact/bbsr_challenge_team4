52018533fcb103ddc85ef72ae3479ddb412eeed3
gustav <gustav.geissler@gmail.com>
2025-05-06 11:06:49 +0200

    add a place for re-embedding functions and embedding data

---
+++ app/functionalities/reembedding.py
@@ -0,0 +1,15 @@
+"""This is the place to put re-embedding functions for after an update for the oekobaudat data was made"""
+
+class ReEmbedder:
+
+    def __init__(self):
+        pass
+
+
+
+
+    def run_reembedding(self):
+        pass
+
+    def create_best_matches_csv(self):
+        pass
